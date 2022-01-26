393503878776d43c9bcc158f6543a71a546314a6
2510juan <98272914+2510juan@users.noreply.github.com>
2022-01-26 15:14:12 +0200

    Add needed imports and variables

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import RadioForm from 'react-native-simple-radio-button';
 
 export default function App() {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(1,'bottle');
+  const [time, setTime] = useState(1, 'hour');
+  const [gender, setGender] = useState('male');
+
+  const bottleAmount = Array();
+  bottleAmount.push({label: '1 bottle', value: 1});
+  bottleAmount.push({label: '2 bottles', value: 2});
+  bottleAmount.push({label: '3 bottles', value: 3});
+  bottleAmount.push({label: '4 bottles', value: 4});
+  bottleAmount.push({label: '5 bottles', value: 5});
+
+  const timeAmount = Array();
+  timeAmount.push({label: '1 hour', value: 1});
+  timeAmount.push({label: '2 hours', value: 2});
+  timeAmount.push({label: '3 hours', value: 3});
+  timeAmount.push({label: '4 hours', value: 4});
+  timeAmount.push({label: '5 hours', value: 5});
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
